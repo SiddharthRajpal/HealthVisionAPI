1dd6ac4770e40155a6279dbe7f9381885275b0a0
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-07-17 19:35:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 @app.route('/braintumor', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/BrainTumuorModel.h5",labelpath="Models/BrainTumuorLabels.txt")
 
     return str(pred)
 
 @app.route('/covid', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/CovidModel.h5",labelpath="Models/CovidLabels.txt")
 
     return str(pred)
 
@@ -21,14 +21,14 @@ def callback():
 @app.route('/glaucoma', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/GlaucomaModel.h5",labelpath="Models/GlaucomaLabels.txt")
 
     return str(pred)
 
 @app.route('/pneumonia', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/Pnemonia.h5",labelpath="Models/labelsPnemonia.txt")
 
     return str(pred)
 
@@ -36,14 +36,14 @@ def callback():
 @app.route('/skincancer', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/SkinCancerModel.h5",labelpath="SkinCancerLabel.txt")
 
     return str(pred)
 
 @app.route('/tuberculosis', methods=['POST'])
 def callback():
     image_file = request.files['image']
-    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+    pred,con = imagerec.imagerecognise(image_file,"Models/tuberculosis_model.h5",labelpath="tb_labels.txt")
 
     return str(pred)
 
