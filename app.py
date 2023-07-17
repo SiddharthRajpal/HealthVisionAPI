9ac7ffa38dcb224af542b0bddac9452ffaf1f0ea
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-07-17 19:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import imagerec
 app = Flask(__name__)
 
 @app.route('/braintumor', methods=['POST'])
-def callback():
+def BrainTumor():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/BrainTumuorModel.h5",labelpath="Models/BrainTumuorLabels.txt")
 
     return str(pred)
 
 @app.route('/covid', methods=['POST'])
-def callback():
+def Covid():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/CovidModel.h5",labelpath="Models/CovidLabels.txt")
 
@@ -19,14 +19,14 @@ def callback():
 
 
 @app.route('/glaucoma', methods=['POST'])
-def callback():
+def Glaucoma():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/GlaucomaModel.h5",labelpath="Models/GlaucomaLabels.txt")
 
     return str(pred)
 
 @app.route('/pneumonia', methods=['POST'])
-def callback():
+def Pnemonia():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/Pnemonia.h5",labelpath="Models/labelsPnemonia.txt")
 
@@ -34,14 +34,14 @@ def callback():
 
 
 @app.route('/skincancer', methods=['POST'])
-def callback():
+def SkinCancer():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/SkinCancerModel.h5",labelpath="SkinCancerLabel.txt")
 
     return str(pred)
 
 @app.route('/tuberculosis', methods=['POST'])
-def callback():
+def Tuberculosis():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"Models/tuberculosis_model.h5",labelpath="tb_labels.txt")
 
