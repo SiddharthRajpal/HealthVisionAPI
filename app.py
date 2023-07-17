b17d8319749db962d50189fa4f83ad84e47890c5
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-07-17 19:29:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,47 @@ import imagerec
 
 app = Flask(__name__)
 
-@app.route('/callback', methods=['POST'])
+@app.route('/braintumor', methods=['POST'])
 def callback():
     image_file = request.files['image']
     pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
 
     return str(pred)
+
+@app.route('/covid', methods=['POST'])
+def callback():
+    image_file = request.files['image']
+    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+
+    return str(pred)
+
+
+@app.route('/glaucoma', methods=['POST'])
+def callback():
+    image_file = request.files['image']
+    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+
+    return str(pred)
+
+@app.route('/pneumonia', methods=['POST'])
+def callback():
+    image_file = request.files['image']
+    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+
+    return str(pred)
+
+
+@app.route('/skincancer', methods=['POST'])
+def callback():
+    image_file = request.files['image']
+    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+
+    return str(pred)
+
+@app.route('/tuberculosis', methods=['POST'])
+def callback():
+    image_file = request.files['image']
+    pred,con = imagerec.imagerecognise(image_file,"BrainTumuorModel.h5",labelpath="BrainTumuorLabels.txt")
+
+    return str(pred)
+
