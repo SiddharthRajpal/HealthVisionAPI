4c56b7f952a7bee7e2e6623defc4e85eebeef542
SiddharthRajpal <siddharthrajpal09@gmail.com>
2023-07-17 22:22:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/braintumor', methods=['POST','PUT'])
 def BrainTumor():
-    image_file = request.files['image']
+    image_data = request.get_data()
+    image_file = base64.b64decode(image_data)
     pred,con = imagerec.imagerecognise(image_file,"Models/BrainTumuorModel.h5",labelpath="Models/BrainTumuorLabels.txt")
 
     return str(pred)
